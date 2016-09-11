a4184e30c1b92b6ac089b583fa04f1a0d6b2ba3e
Fidel Hernandez <fidel.hernandez002@gmail.com>
2016-09-10 20:05:29 -0400

    Add string error handling

--- ProgrammingAssignment1/src/programmingassignment1/ProgrammingAssignment1.java
+++ ProgrammingAssignment1/src/programmingassignment1/ProgrammingAssignment1.java
@@ -1,4 +1,14 @@
 
+//******************************************************************************
+// PantherID:  [6045594]
+// CLASS: COP 2210 – [Fall 2016]
+// ASSIGNMENT # [#1]
+// DATE: [09/14/2016]
+//
+// I hereby swear and affirm that this work is solely my own, and not the work 
+// or the derivative of the work of someone else.
+//******************************************************************************
+
 package programmingassignment1;
 
 import java.util.Scanner;
@@ -15,39 +25,37 @@ public class ProgrammingAssignment1 {
         
         System.out.println("If you want to see the lotto winning numbers " +
                            "type lotto");
-        System.out.println("If you want to see the fantasy five winning numbers " +
-                           "type fantasy");
+        System.out.println("If you want to see the fantasy five winning " +
+                           " numbers type fantasy");
         System.out.print("If you want to see both type both: ");
         
         String userInput = in.nextLine();
+        userInput.toLowerCase();
         
-        if (userInput.equals("lotto"))
-        {
-            System.out.print("The lotto winning numbers are ");
-            lottoTicket.showNumbers();
-            System.out.println();
+        switch (userInput) {
+            case "lotto":
+                System.out.print("The lotto winning numbers are ");
+                lottoTicket.showNumbers();
+                System.out.println();
+                break;
+            case "fantasy":
+                System.out.print("The fantasy 5 winning numbers are ");
+                fantasyTicket.showNumbers();
+                System.out.println();
+                break;
+            case "both":
+                System.out.print("The lotto winning numbers are ");
+                lottoTicket.showNumbers();
+                System.out.println();
+                System.out.print("The fantasy 5 winning numbers are ");
+                fantasyTicket.showNumbers();
+                System.out.println();
+                break;
+            default:
+                System.out.println("Sorry I couldn't recognize your  command " +
+                        "please try again!");
+                break;
         }
-                               
-        else if (userInput.equals("fantasy"))
-        {
-            System.out.print("The fantasy 5 winning numbers are ");
-            fantasyTicket.showNumbers();
-            System.out.println();
-        }
-                               
-        else if (userInput.equals("both"))
-        {
-            System.out.print("The lotto winning numbers are ");
-            lottoTicket.showNumbers();
-            System.out.println();
-                               
-            System.out.print("The fantasy 5 winning numbers are ");
-            fantasyTicket.showNumbers();
-            System.out.println();
-        }
-        else
-            System.out.println("Sorry I couldn't recognize your  command " +
-                               "please try again!");
                                
         System.out.println("Thank you for playing!");
     }
